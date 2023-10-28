3e91e1ffd7c1e089f61dbd3e49fc016ac2d4e5c0
jagtapswati <131947132+jagtapswati@users.noreply.github.com>
2023-10-28 07:01:33 -0700

    Add files via upload

---
+++ IdentifiersDemo1.java
@@ -0,0 +1,20 @@
+public class IdentifiersDemo1 {
+
+	public static void main(String[] args) {
+
+		//Valid identifiers
+
+		int no=12;
+		int no2=133;
+		float no3=56.88f;
+		double _no4=45367.99d;
+		int $no2=133;
+
+		//invalid identifiers
+		//int no 7=45;
+	//	int #no8=23;
+		//int for=89;
+
+	}
+
+}
